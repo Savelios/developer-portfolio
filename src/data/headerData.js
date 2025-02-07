965eeaa6a277d47ee8d7b99944cc33c80edd27b0
Savelios <avel_stefan@outlook.com>
2025-02-07 16:26:04 +0300

    add

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,12 @@
-import resume from '../assets/pdf/resume.pdf'
+import resume from "../assets/pdf/resume.pdf";
+import LandingImg from "../assets/png/LandingImg.png";
 
 export const headerData = {
-    name: 'Manolis Ka',
-    title: "Web Developer",
-    desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-    image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
-    resumePdf: resume
-}
+  name: "Manolis Ka",
+  title: "Web Developer",
+  desciption:
+    "There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
+  // image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
+  resumePdf: resume,
+  image: LandingImg,
+};
